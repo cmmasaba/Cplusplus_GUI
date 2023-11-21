956a56434ef97ee805d7c7aaa8594dcdc11fa183
Collins Mmasaba <mmasabacollins9@gmail.com>
2023-11-21 07:41:10 +0300

    Making connections between objects

---
+++ chapter1/connections/connection.cpp
@@ -0,0 +1,11 @@
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QPushButton>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QPushButton *button = new QPushButton("Quit");
+    QObject::connect(button, SIGNAL(clicked()), &app, SLOT(quit()));
+    button->show();
+    return app.exec();
+}
